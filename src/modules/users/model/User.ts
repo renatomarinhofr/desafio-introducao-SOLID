9c114aa4802f068a48702c66803b182504d59ed2
Renato Marinho <75874462+renatomarinhofr@users.noreply.github.com>
2021-07-18 02:13:33 -0300

    Update User.ts

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -3,16 +3,16 @@ import { v4 as uuidV4 } from "uuid";
 class User {
   id?: string;
   name: string;
-  admin?: boolean;
+  admin: boolean;
   email: string;
   created_at: Date;
   updated_at: Date;
 
   constructor() {
-    if (!this.id && !this.admin) {
+    if (!this.id) {
       this.id = uuidV4();
-      this.admin = false;
     }
+    this.admin = false;
   }
 }
 
